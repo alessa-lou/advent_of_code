f32f32fec363e746205e9aa6de52ea81480e0eaa
alessa-lou <alessa_hardwick@live.co.uk>
2020-12-08 16:42:43 +0000

    day 1 part 1 solved

--- day_1/expense_report.py
+++ day_1/expense_report.py
@@ -1,5 +1,38 @@
 class ExpenseReport():
-    
-    def add_nums(self):
-      num = 1
-      return num
+  
+  def __init__(self):
+    self.res = []
+
+  def read_input(self):
+    f = open("puzzle_input.txt", "r")
+    file_content = [int(line.strip()) for line in f.readlines()]
+    # print(file_content)
+    return file_content
+  
+  def add_nums(self):
+    file_content = self.read_input()
+    print("printing file_content")
+    print(file_content)
+    print(type(file_content))
+    for i, val1 in enumerate(file_content):
+      for j, val2 in enumerate(file_content[i+1:]):
+        if val1 + val2 == 2020:
+          print(val1)
+          print(val2)
+          self.res.append(val1 + val2)
+          print(self.res)
+          answer = val1 * val2
+          print(answer)
+          return answer
+
+
+
+    # for i in range(0, len(file_content), 2):
+    #   res.append(file_content[i] + file_content[i + 1])
+    #   counter+=1
+    # print("printing res")
+    # print(res)
+    # print(counter)
+    # return res
+
+   
--- day_1/tests/test_expense_report.py
+++ day_1/tests/test_expense_report.py
@@ -1,7 +1,17 @@
 import pytest
 from expense_report import ExpenseReport
 
-def test_add_nums():
+@pytest.fixture
+def er():
     er = ExpenseReport()
+    return er
+
+def test_read_input(er):
+    file_content = er.read_input()
+    assert type(file_content) is list
+
+def test_add_nums(er):
     sum = er.add_nums()
     assert type(sum) is int
+    assert sum == 878724
+    assert er.res == [2020]
